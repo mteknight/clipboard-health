7c99bc766c5b1a23e232ea2df4487d66aea68273
Alexandre Fernandes <mteknight@gmail.com>
2023-01-14 20:37:35 -0300

    cleanup

--- dpk.test.js
+++ dpk.test.js
@@ -3,11 +3,14 @@ const { deterministicPartitionKey } = require("./dpk");
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
+
     expect(trivialKey).toBe("0");
   });
+
   it("returns a string of hexadecimal characters when given no partition key", () => {
     const event = { data: "sampleData" };
     const result = deterministicPartitionKey(event);
+
     expect(typeof result).toBe("string");
     expect(result).toMatch(/^[a-f0-9]+$/);
   });
@@ -15,13 +18,15 @@ describe("deterministicPartitionKey", () => {
   it("returns the partition key when given a partition key with less than 256 characters", () => {
     const event = { partitionKey: "samplePartitionKey", data: "sampleData" };
     const result = deterministicPartitionKey(event);
+
     expect(result).toBe(event.partitionKey);
   });
 
-  test("returns a string of hexadecimal characters when given a partition key that exceeds 256 characters", () => {
+  it("returns a string of hexadecimal characters when given a partition key that exceeds 256 characters", () => {
     const longString = "a".repeat(257);
     const event = { partitionKey: longString, data: "sample" };
     const result = deterministicPartitionKey(event);
+
     expect(typeof result).toBe("string");
     expect(result).toMatch(/^[a-f0-9]+$/);
   });
