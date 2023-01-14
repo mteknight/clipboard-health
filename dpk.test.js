a8165aa4938e26ff8f559a4e0437e49d7632130d
Alexandre Fernandes <mteknight@gmail.com>
2023-01-14 20:14:31 -0300

    addes tests to cover all features before refactoring.

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,26 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+  it("returns a string of hexadecimal characters when given no partition key", () => {
+    const event = { data: "sampleData" };
+    const result = deterministicPartitionKey(event);
+    expect(typeof result).toBe("string");
+    expect(result).toMatch(/^[a-f0-9]+$/);
+  });
+
+  it("returns the partition key when given a partition key with less than 256 characters", () => {
+    const event = { partitionKey: "samplePartitionKey", data: "sampleData" };
+    const result = deterministicPartitionKey(event);
+    expect(result).toBe(event.partitionKey);
+  });
+
+  test("returns a string of hexadecimal characters when given a partition key that exceeds 256 characters", () => {
+    const longString = "a".repeat(257);
+    const event = { partitionKey: longString, data: "sample" };
+    const result = deterministicPartitionKey(event);
+    expect(typeof result).toBe("string");
+    expect(result).toMatch(/^[a-f0-9]+$/);
+  });
 });
+
+
