fb6110a61afaa5a07719e19da97436b2c643cf8b
Alexandre Fernandes <mteknight@gmail.com>
2023-01-14 20:15:51 -0300

    refactored the code to adhere to best practices and principles

--- dpk.js
+++ dpk.js
@@ -1,28 +1,35 @@
 const crypto = require("crypto");
 
+
+const createHash = (data) => {
+    const algorithm = "sha3-512";
+    const encoding = "hex";
+
+    return crypto
+        .createHash(algorithm)
+        .update(data)
+        .digest(encoding);
+};
+
+const createHashFromEvent = event => {
+    let data = JSON.stringify(event);
+    return createHash(data);
+};
+
+const getPartitionKey = (event) => {
+    const TRIVIAL_PARTITION_KEY = "0";
+    if (!event) {
+        return TRIVIAL_PARTITION_KEY;
+    }
+
+    return event.partitionKey ?? createHashFromEvent(event);
+};
+
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+    const MAX_PARTITION_KEY_LENGTH = 256;
+    let partitionKey = getPartitionKey(event);
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+    return partitionKey.length > MAX_PARTITION_KEY_LENGTH
+        ? createHash(partitionKey)
+        : partitionKey;
 };
